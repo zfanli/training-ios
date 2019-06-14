d8506eceaa613ddf05fdf74a9a2bbf754de83362
Richard <zfanli@outlook.com>
2019-06-14 22:47:53 +0800

    Initial Commit

---
+++ trainingios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trainingios
+//
+//  Created by Richard Zeng on 2019/6/14.
+//  Copyright © 2019 Richard Zeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
